19181ff4bb0196b45d5c29dc3306492d245adea6
Amin Khorsandiaghai <aminkhorsandi@gmail.com>
2015-02-11 21:38:10 +0100

    Fixed README and also some code change

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ MLCCompiler
 ===========
 
 A MuC compiler written in StandardML.
+
+Example usage:
+
+    ./ucc.sh testsuit/noisy/simple/sim09.c
+
+will create a sim09.s assembly file.
+
+To compile and run it you have to use a MIPS emulator such as SPIM:
+
+    spim -file sim09.s 
+
+should give the output:
+
+    12345678
+    ABCD
--- ucc.sh
+++ ucc.sh
@@ -2,16 +2,31 @@
 ARCH=unknown
 OPSYS=unknown
 case `uname -s` in
-SunOS)	case `uname -r` in
-	5.*)	OPSYS=solaris;;
+SunOS)	
+    case `uname -r` in
+	5.*)	
+        OPSYS=solaris
+    ;;
 	esac
 	case `uname -m` in
-	sun4*)	ARCH=sparc;;
-	i86pc)	ARCH=x86;;
-	esac;;
+	sun4*)	
+        ARCH=sparc
+    ;;
+	i86pc)	
+        ARCH=x86
+    ;;
+	esac
+;;    
 Linux)	OPSYS=linux
 	case `uname -m` in
-	i[3456]86)	ARCH=x86;;
-	esac;;
+	i[3456]86)	
+        ARCH=x86
+        ;;
+	x86_64)	
+        ARCH=x86
+        ;;
+	esac
+;;
 esac
+
 exec sml @SMLload=ucc.$ARCH-$OPSYS $*
